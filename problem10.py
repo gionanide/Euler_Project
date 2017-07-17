050f9bca7dd6e1839c982b937e0c63e4ee8d1721
Manos Gionanidis <gionanidis32@gmail.com>
2017-07-17 12:52:22 +0300

    Add files via upload

---
+++ problem10.py
@@ -0,0 +1,12 @@
+#!usr/bin/python
+sum=0
+n=2000000
+for p in range(2, n+1):
+	for i in range(2, p):
+        	if p % i == 0:
+            		break
+    	else:
+        	sum+=p
+		print sum
+print sum
+
