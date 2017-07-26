b7db5a5fa29f319bf97433fbb2cb5b379f145df1
Manos Gionanidis <gionanidis32@gmail.com>
2017-07-26 12:08:16 +0300

    Add files via upload

---
+++ problem17.py
@@ -0,0 +1,34 @@
+#!usr/bin/python
+#Numbers 1-9
+sum1 =3 + 3 + 5 +4 + 4 +3 +5 +5 +4
+print sum1
+#Numbers 10-19
+sum2=3 + 6 + 6 + 8 + 8 + 7 + 7 + 9 + 8 + 8
+print sum2
+#Numbers 20-99
+sum3=10*(6 + 6 + 5 + 5 + 5 + 7 + 6 + 6) + 8*36
+print sum3
+'''Number 100-999
+If we break it down we have the numbers 1-9 occurring 100 times each.  36*100 = 3600
+1-99 occurring 9 times  9*854 = 7686
+We have “hundred” occurring 9 times with 7 letters.  7*9 = 63
+We have “hundred and” occurring 9*99 times with 10 letters  9*99*10 = 8910
+So in total we have'''
+sum4=3600 + 7686 + 63 + 8910
+print sum4
+#Number one thousand 
+sum5=11
+print sum5
+#Amount
+amount=sum1+sum2+sum3+sum4+sum5
+print amount
+	
+	
+	
+			
+	
+
+
+		
+
+		
