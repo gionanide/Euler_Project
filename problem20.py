4e77ebca73e104d0ab2add0fba4b4a62d9bdc527
Manos Gionanidis <gionanidis32@gmail.com>
2017-07-26 11:51:01 +0300

    Add files via upload

---
+++ problem20.py
@@ -0,0 +1,23 @@
+#!usr/bin/python
+sum=1
+for i in range(100,0,-1):
+	sum*=i
+amount = list(str(sum))
+sumDigits=0
+for x in range(len(amount)):
+	sumDigits+=int(amount[x])
+print sumDigits
+	
+	
+    
+
+	
+	
+	
+			
+	
+
+
+		
+
+		
